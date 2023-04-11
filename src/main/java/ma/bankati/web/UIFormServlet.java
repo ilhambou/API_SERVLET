ad822c17def0686471b3fabf7ec37c71147b6afc
ilhambou <82772375+ilhambou@users.noreply.github.com>
2023-04-11 20:51:36 +0000

    Formulaire

---
+++ src/main/java/ma/bankati/web/UIFormServlet.java
@@ -0,0 +1,29 @@
+package ma.bankati.web;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/form")
+public class UIFormServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+        out.println("Nom : " + request.getParameter("nom"));
+        out.println("Prenom : "+ request.getParameter("prenom"));
+        if(request.getParameterValues("radio1")[0].equals("mas"))
+            out.print("C'est un homme. Il");
+        else
+            out.print("C'est une femme. Elle");
+
+        out.print(" a ecrit : ");
+        out.println(request.getParameter("textarea"));
+
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,32 @@
+<!DOCTYPE html>
 <html>
-<body>
-<h2>Hello World!</h2>
+<head>
+    <meta charset="UTF-8">
+    <title>UI :: FORM</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css.monStyle.css">
+</head>
+<body style="font-family: Optima" class="mg20"><br>
+<p class="h4 bold gray">Formulaire</p><br>
+<form name="form1" method="get" action="form">
+    <p class="bold">
+        Nom : <input type="text" name="nom">
+        Prenom : <input type="text" name="prenom">
+
+    </p>
+    <p><span class="bold">Sexe :</span>
+        <input type="radio" name="radio1" value="mas" checked> M
+        <input type="radio" name="radio1" value="fem"> F
+
+    </p>
+    <p class="bold"> Commentaire : <br>
+        <textarea name="textarea" cols="56" rows="10" class="gray"></textarea>
+        <br><br>
+        <input class="btn btn-success bold" type="submit" name="Submit" value="Soumettre">
+
+
+    </p>
+</form>
+
 </body>
 </html>
