2025733726cbbca1373c46bb78937fdc01977609
ilhambou <82772375+ilhambou@users.noreply.github.com>
2023-04-11 20:12:40 +0000

    helloServlet

---
+++ src/main/java/ma/bankati/web/HelloServlet.java
@@ -0,0 +1,25 @@
+package ma.bankati.web;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+//@WebServlet(name = "HS",urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        var out =response.getWriter();
+        out.println("<h1>Hello from HelloServlet ^_^ </h1>");
+        out.println("<h2> Protocole utilisé : "+request.getProtocol()+"</h2>");
+        out.println("<h2> Methode utilisé : "+request.getMethod()+"</h2>");
+        out.println("<h2> nom utilisé : "+request.getServerName()+"</h2>");
+        out.println("<h2> chemin utilisé : "+request.getServletPath()+"</h2>");
+        out.println("<h2> port utilisé : "+request.getServerPort()+"</h2>");
+
+    }
+}
